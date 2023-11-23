7ff44d66be64a36715be6d0890141caf43d0c0a7
ununian <ununian@outlook.com>
2023-11-23 15:31:08 +0800

    update

--- packages/core/src/extensions/Blocks/nodes/BlockContainer.ts
+++ packages/core/src/extensions/Blocks/nodes/BlockContainer.ts
@@ -5,7 +5,7 @@ import {
   blockToNode,
   inlineContentToNodes,
 } from "../../../api/nodeConversions/nodeConversions";
-import { isInTable } from 'prosemirror-tables'
+import { isInTable } from "prosemirror-tables";
 
 import {
   BlockNoteDOMAttributes,
@@ -402,7 +402,7 @@ export const BlockContainer = Node.create<{
               state.doc,
               state.selection.from
             )!;
-            
+
             const selectionAtBlockStart =
               state.selection.$anchor.parentOffset === 0;
             const isParagraph = contentType.name === "paragraph";
@@ -462,6 +462,51 @@ export const BlockContainer = Node.create<{
               depth === 2
             ) {
               return commands.BNMergeBlocks(posBetweenBlocks);
+            }
+
+            return false;
+          }),
+      ]);
+
+    const handleDelete = () =>
+      this.editor.commands.first(({ commands }) => [
+        // Deletes the selection if it's not empty.
+        () => commands.deleteSelection(),
+        // Merges block with the next one (at the same nesting level or lower),
+        // if one exists, the block has no children, and the selection is at the
+        // end of the block.
+        () =>
+          commands.command(({ state }) => {
+            const { node, contentNode, depth, endPos } = getBlockInfoFromPos(
+              state.doc,
+              state.selection.from
+            )!;
+
+            const blockAtDocEnd = false;
+            const selectionAtBlockEnd =
+              state.selection.$anchor.parentOffset ===
+              contentNode.firstChild!.nodeSize;
+            const selectionEmpty =
+              state.selection.anchor === state.selection.head;
+            const hasChildBlocks = node.childCount === 2;
+
+            if (
+              !blockAtDocEnd &&
+              selectionAtBlockEnd &&
+              selectionEmpty &&
+              !hasChildBlocks
+            ) {
+              let oldDepth = depth;
+              let newPos = endPos + 2;
+              let newDepth = state.doc.resolve(newPos).depth;
+
+              while (newDepth < oldDepth) {
+                oldDepth = newDepth;
+                newPos += 2;
+                newDepth = state.doc.resolve(newPos).depth;
+              }
+
+              return commands.BNMergeBlocks(newPos - 1);
             }
 
             return false;
@@ -580,6 +625,7 @@ export const BlockContainer = Node.create<{
     return {
       Backspace: handleBackspace,
       Enter: handleEnter,
+      Delete: handleDelete,
       // Always returning true for tab key presses ensures they're not captured by the browser. Otherwise, they blur the
       // editor since the browser will try to use tab for keyboard navigation.
       Tab: () => {
