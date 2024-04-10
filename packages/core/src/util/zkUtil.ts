48414e52cc007fa3b99174e0beed41212caa44e7
tangshengzhi <tangshengzhi@zhikan.net.cn>
2024-04-10 17:22:41 +0800

    util fix

--- packages/core/src/util/zkUtil.ts
+++ packages/core/src/util/zkUtil.ts
@@ -1,6 +1,6 @@
 export function getEditorWrapper(e: HTMLElement | null) {
   let el = e;
-  while(el && !el.classList.contains('.document-editor')) {
+  while(el && !el.classList.contains('document-editor')) {
     el = el.parentElement;
   }
   return el || e;
