40a98e68fae2bdc8220c7fc8ae60d7de5d54c223
tangshengzhi <tangshengzhi@zhikan.net.cn>
2024-04-12 14:38:47 +0800

    联网搜索部分迁移

--- packages/core/src/api/parsers/markdown/parseMarkdown.ts
+++ packages/core/src/api/parsers/markdown/parseMarkdown.ts
@@ -47,6 +47,32 @@ function code(state: any, node: any) {
   return result;
 }
 
+function decryptLink(encryptedLink: string): string {
+  const KEY = 42; // 解密密钥，必须与加密密钥相同
+  let decryptedLink = "";
+  for (let i = 0; i < encryptedLink.length; i++)
+    decryptedLink += String.fromCharCode(encryptedLink.charCodeAt(i) ^ KEY);
+
+  return decryptedLink;
+}
+export function decryptLinks(text: string) {
+  const regex = /\{\{\{(\d+)-(.+?)\}\}\}/g;
+  return text.replaceAll(regex, (match, citationNumber, encryptedLink) => {
+    const decryptedLink = decryptLink(encryptedLink); // 解密链接
+    return `{{{${citationNumber}-${decryptedLink}}}}`;
+  });
+}
+export function transformCitation(text: string) {
+  const decryptText = decryptLinks(text);
+  return decryptText?.replaceAll(
+    /\{\{\{(\d+)-(.+?)\}\}\}/g,
+    (match, citationNumber, encryptedLink) => {
+      return `<span data-type='inlineTips' data-tip-link='${encryptedLink}' data-tip-id='${citationNumber}'>${citationNumber}</span>`;
+    }
+  );
+}
+
+
 export function markdownToBlocks<
   BSchema extends BlockSchema,
   I extends InlineContentSchema,
@@ -71,7 +97,8 @@ export function markdownToBlocks<
     .processSync(markdown);
 
   return HTMLToBlocks(
-    htmlString.value as string,
+    // htmlString.value as string,
+    transformCitation(htmlString.value as string),
     blockSchema,
     icSchema,
     styleSchema,
